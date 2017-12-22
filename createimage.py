53d03913b7ab28820c00ab1a43280fc271b815fc
Jussi Pakkanen <jpakkane@gmail.com>
2017-12-22 23:08:17 +0200

    Build base chroot image.

--- createimage.py
+++ createimage.py
@@ -15,7 +15,27 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import os, sys, subprocess
+import os, sys, subprocess, shutil
+
+class ImageCreator:
+    
+    def __init__(self):
+        self.chrootdir = 'rootdir'
+        self.basetar = 'baseroot'
+        self.basetarfname = 'baseroot.tar.xz'
+        self.distro = 'stretch'
+
+    def build_base_image(self):
+        if os.path.exists(self.basetarfname):
+            return
+        if os.path.exists(self.chrootdir):
+            shutil.rmtree(self.chrootdir)
+        os.mkdir(self.chrootdir)
+        subprocess.check_call(['debootstrap', self.distro, self.chrootdir, 'http://ftp.fi.debian.org/debian'])
+        shutil.make_archive(self.basetar, 'xztar', '.', self.chrootdir)
 
 if __name__ == '__main__':
-    pass
+    ic = ImageCreator()
+    if os.getuid() != 0:
+        sys.exit('This script must be run with root privileges.')
+    ic.build_base_image()
